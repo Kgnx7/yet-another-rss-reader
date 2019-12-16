5c651a5f57d5731003727e6b41117527ac2c8579
Kgnx7 <ukqfhf@yandex.ru>
2019-12-16 16:49:23 +0300

    add basic style to rss-channel-form

--- src/RssChannelForm.vue
+++ src/RssChannelForm.vue
@@ -1,24 +1,28 @@
 <template>
-  <div id="rss-channel-form" class="">
-     placeholder for form
-  </div>
+  <details id="rss-channel-form" class="rss-channel-form">
+    <summary class="rss-channel-form__summary">Add new RSS channel</summary>
+
+    <form class="rss-channel-form__form">
+      <!-- <label for="title">Title</label> -->
+      <input type="text" id="name" placeholder="title">
+      <!-- <label for="link">link</label> -->
+      <input type="text" id="link" placeholder="link">
+      <input type="submit" value="Add new RSS channel" class="square-button">
+    </form>
+  </details>
 </template>
 
 <script>
 export default {
   name: 'rss-channel-form',
-  components: {
-  
-  },
-  props: {
-
-  },
-  methods: {
-  
-  },
 }
 </script>
 
 <style>
- 
+  .rss-channel-form__summary {
+    cursor: pointer;
+  }
+  .rss-channel-form__form {
+    padding-top: 15px;
+  }
 </style>
