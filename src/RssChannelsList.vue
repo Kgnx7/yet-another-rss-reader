ef38dcf3b7455a6d594644a4d39aa98f18a8725d
Kgnx7 <ukqfhf@yandex.ru>
2019-12-16 09:55:17 +0300

    add RssChannelsList.vue file

---
+++ src/RssChannelsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <aside id="rss-channels-list">
+    <nav>
+      <ul>
+        <li v-for="channel in channels" v-bind:key="channel.id">
+          {{ channel.title }}
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'rss-channels-list',
+  props: {
+    channels: Array,
+  },
+}
+</script>
+
+<style>
+  
+</style>
