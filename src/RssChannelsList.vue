e3dfa3378af77d1dd64f6c3ff9d26ae37a2042bd
Kgnx7 <ukqfhf@yandex.ru>
2019-12-18 19:16:21 +0300

    add details options with delete btn

--- src/RssChannelsList.vue
+++ src/RssChannelsList.vue
@@ -2,8 +2,29 @@
   <nav id="rss-channels-list">
     <ul class="rss-channels-list__list">
       <li v-for="channel in channels" :key="channel.id" class="rss-channels-list__item">
-        <button class="rss-channels-list__button muted-button square-button" @click="handleBtnClick(channel.id)">{{ channel.title }}</button>
-        <a :href="channel.link"><small class="rss-channels-list__link">{{ channel.link }}</small></a>
+
+        <button 
+          class="rss-channels-list__button full-button muted-button square-button"
+          @click="handleSetChannel(channel.id)"
+        >
+          {{ channel.title }}
+        </button>
+
+        <details class="rss-channels-list__options">
+          <summary>Details</summary>
+          <div class="options__wrapper">
+            <!-- <p>Rss channel link: <a :href="channel.link"><small class="rss-channels-list__link">{{ channel.link }}</small></a></p> -->
+            <p class="rss-channels-link__p">Rss channel link: <a :href="channel.link"><small class="rss-channels-list__link">{{ channel.link }}</small></a></p>
+
+            <button 
+              class="rss-channels-list__button rss-channels-list__button--remove muted-button square-button" 
+              @click="handleDeleteChannel(channel.id)"
+            >
+              Remove channel
+            </button>
+
+          </div>
+        </details>
       </li>
     </ul>
   </nav>
@@ -16,8 +37,11 @@ export default {
     channels: Array,
   },
   methods: {
-    handleBtnClick(id) {
-      this.$emit('handleChannelClick', id);
+    handleSetChannel(id) {
+      this.$emit('handleSetChannel', id);
+    },
+    handleDeleteChannel(id) {
+      this.$emit('handleDeleteChannel', id);
     }
   }
 }
@@ -34,11 +58,27 @@ export default {
     /* padding-right: 15px; */
   }
   .rss-channels-list__link {
-    display: block;
+    /* display: block; */
   }
   .rss-channels-list__button {
     margin: 0;
     transition: .25s;
-    width: 100%;
+  }
+
+  .rss-channels-list__button--remove {
+    border-color: coral !important;
+  }
+
+  .rss-channels-list__options {
+    cursor: pointer;
+    margin-top: 5px;
+  }
+
+  .options__wrapper {
+    padding-top: 15px;
+  }
+
+  .rss-channels-link__p {
+    margin-bottom: 10px;
   }
 </style>
